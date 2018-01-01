bd3798e5271720dd0b45193aba87648d20a12b10
jonathan <jonathan.grondin@epitech.eu>
2018-01-02 00:10:38 +0100

    MyNetflix

---
+++ src/index.js
@@ -0,0 +1,8 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './containers/app'
+
+const API_KEY = "api_key=[68419a42db6203305b6e65b5a77bf747]";
+
+ReactDOM.render(<App/>,document.querySelector('.container'));
